b256d413947b42a8989cd5fbd00505f5e12be10f
Randi Schreiner <mimirandi21@gmail.com>
2021-01-16 11:36:41 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 61;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.random();
+
+let num3 = 20;
+let num4 = 5;
+let mod = num3 / num4;
+
+let max = Math.max(7, 9, 14, 20);
